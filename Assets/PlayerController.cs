9b3cf9e835eee1eee1e538bcf70bb6af0d33cc2c
Student <Student@aii.stu>
2018-07-26 19:50:54 -0400

    Wienrz Initial Commit
    Unity project with main scene. Boxes as placeholders controlled separately (wasd + space and arrows + end).

---
+++ Assets/PlayerController.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    void Update()
+    {
+
+        if (gameObject.tag == "PlayerBack")
+        {
+            var x = Input.GetAxis("HorizontalBack") * Time.deltaTime * 150.0f;
+            var y = Input.GetAxis("JumpBack") * Time.deltaTime * 10.0f;
+            var z = Input.GetAxis("VerticalBack") * Time.deltaTime * 3.0f;
+
+            transform.Rotate(0, x, 0);
+            transform.Translate(0, y, 0);
+            transform.Translate(0, 0, z);
+        }
+
+        if (gameObject.tag == "PlayerFront")
+        {
+            var x = Input.GetAxis("HorizontalFront") * Time.deltaTime * 150.0f;
+            var y = Input.GetAxis("JumpFront") * Time.deltaTime * 10.0f;
+            var z = Input.GetAxis("VerticalFront") * Time.deltaTime * 3.0f;
+
+            transform.Rotate(0, x, 0);
+            transform.Translate(0, y, 0);
+            transform.Translate(0, 0, z);
+        }
+    }
+}
