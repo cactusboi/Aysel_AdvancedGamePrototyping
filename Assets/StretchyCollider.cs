8a899849ed1edd9856e941aadaf5949f5047e437
Student <Student@aii.stu>
2018-09-06 21:24:29 -0400

    in class changes

---
+++ Assets/StretchyCollider.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StretchyCollider : MonoBehaviour {
+
+    public Transform FrontBone;
+    public Transform BackBone;
+    public BoxCollider ColliderToStretch;
+    public float StretchRatio;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        ColliderToStretch.size = new Vector3(Vector3.Distance(FrontBone.position, BackBone.position) * StretchRatio, ColliderToStretch.size.y, ColliderToStretch.size.z);
+
+	}
+}
