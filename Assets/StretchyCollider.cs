6257834bc89fabb3bdf7f3b7e9c7408d8c3eae02
cactusboi <36212691+cactusboi@users.noreply.github.com>
2018-09-19 16:57:53 -0400

    update to scene and stretch coll.
    worked on obstacle course
    added colliders so player has specfied area to stay in
    stretchy collider cooperates

--- Assets/StretchyCollider.cs
+++ Assets/StretchyCollider.cs
@@ -21,17 +21,17 @@ public class StretchyCollider : MonoBehaviour {
 	void Update () {
 
         ColliderToStretch.size = new Vector3(ColliderToStretch.size.x, ColliderToStretch.size.y, Vector3.Distance(FrontBone.position, BackBone.position) * StretchRatio);
-        //MidBoneTrans.position = ((FrontBone.transform.position + BackBone.transform.position) / 2);
-        //MidCollider.position = MidBone.position;
+        MidBoneTrans.position = ((FrontBone.transform.position + BackBone.transform.position) / 2);
+        MidCollider.position = MidBone.position;
         //navmesh dude to the position of midbone.position in the update
 
 
     }
 
-    private void FixedUpdate()
+    /*private void FixedUpdate()
     {
         MidBone.MovePosition(((FrontBone.transform.position + BackBone.transform.position) / 2) * (20 * Time.deltaTime));
-    }
+    }*/
 }
 
     
